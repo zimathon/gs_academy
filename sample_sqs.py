f2da9a36f5eeee0d076af99d56168e07bf67fc39
Yusuke Sasajima <zimathon@gmail.com>
2019-06-10 22:52:40 +0900

    add sample sqs.py

---
+++ sample_sqs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import boto3
+from time import time
+
+# Get the service resource
+sqs = boto3.resource('sqs')
+
+# Create the queue. This returns an SQS.Queue instance
+queue = sqs.create_queue(QueueName='test', Attributes={'DelaySeconds': '5'})
+
+# You can now access identifiers and attributes
+print(queue.url)
+print(queue.attributes.get('DelaySeconds'))
