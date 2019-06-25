71fa6f3b92a7a39966e159f00e3e717a9f531bef
Yusuke Sasajima <zimathon@gmail.com>
2019-06-25 10:26:58 +0900

    rename queue name

--- sample_sqs.py
+++ sample_sqs.py
@@ -6,7 +6,7 @@ from time import time
 sqs = boto3.resource('sqs')
 
 # Create the queue. This returns an SQS.Queue instance
-queue = sqs.create_queue(QueueName='test', Attributes={'DelaySeconds': '5'})
+queue = sqs.create_queue(QueueName='gs', Attributes={'DelaySeconds': '5'})
 
 # You can now access identifiers and attributes
 print(queue.url)
