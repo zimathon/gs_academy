72617c09defee9618e3ae13c96155456ebe1ee19
Yusuke Sasajima <zimathon@gmail.com>
2019-06-26 12:05:05 +0900

    enqueue multiprocess

--- sample_enqueue_sqs.py
+++ sample_enqueue_sqs.py
@@ -2,6 +2,7 @@
 import boto3
 import json
 from time import time
+import multiprocessing
 
 name = 'gs'
 sqs = boto3.resource('sqs')
@@ -12,10 +13,16 @@ except:
     # 指定したキューがない場合はexceptionが返るので、キューを作成
     queue = sqs.create_queue(QueueName=name)
  
-message = '{"email":"test@gmail.com","html_body":"&lt;html&gt;test&lt;/html&gt;","text_body":""}'
- 
-# メッセージ×3をキューに送信
-msg_num = 3
-msg_list = [{'Id' : '{}'.format(i+1), 'MessageBody' : json.dumps(message)} for i in range(msg_num)]
-response = queue.send_messages(Entries=msg_list)
-print(response)
+def enqueue():
+    message = '{"email":"test@gmail.com","html_body":"&lt;html&gt;test&lt;/html&gt;","text_body":""}'
+    
+    # メッセージ×3をキューに送信
+    msg_num = 3
+    msg_list = [{'Id' : '{}'.format(i+1), 'MessageBody' : json.dumps(message)} for i in range(msg_num)]
+    response = queue.send_messages(Entries=msg_list)
+    print(response)
+
+if __name__ == '__main__':    
+    for n in range(1000):
+        p = multiprocessing.Process(target=enqueue,args=())
+        p.start()
